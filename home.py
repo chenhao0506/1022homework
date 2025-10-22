1bcc7932f9b16f822379b4ed9a2bf01ad7af42cd
chenhao0506 <dannyfann0506@gmail.com>
2025-10-22 03:37:31 +0000

    home3

--- home.py
+++ home.py
@@ -2,3 +2,14 @@ import streamlit as st
 st.title("范宸晧")
 st.header("范宸晧的GIS專題 in 1022")
 st.write("我是地理三的范宸晧，這個APP是用來...")
+
+pages = [
+    st.Page('page_map_viewer.py', title = '互動地圖瀏覽')
+    ]
+
+with st.sidebar:
+st.title("App 導覽")
+# st.navigation() 會回傳被選擇的頁面
+selected_page = st.navigation(pages)
+
+selected_page.run()
