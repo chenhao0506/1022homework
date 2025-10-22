d364215cd4c9b730555224e4c54071b18dba149b
chenhao0506 <dannyfann0506@gmail.com>
2025-10-22 09:42:47 +0000

    map4

--- page_map.py
+++ page_map.py
@@ -2,18 +2,19 @@ import streamlit as st
 import leafmap.foliumap as leafmap
 import geopandas as gpd # 引入 GeoPandas
 
+option = st.selectbox("請選擇底圖",("OpenTopoMap","Esri.EorldImagery","CartoDB.DarkMatter"))
+
 st.set_page_config(layout="wide")
 st.title("Leafmap + GeoPandas (向量)")
 
 # --- 1. 用 GeoPandas 讀取資料 ---
-# 這是 Natural Earth 110m cultural vectors 的官方託管 .zip 檔案連結
-url = "https://naciscdn.org/naturalearth/110m/cultural/ne_110m_admin_0_countries.zip"
+url = "路外停車資訊.json"
 
-# GeoPandas 可以直接從 URL 讀取 .zip 檔
+# GeoPandas 可以直接從 URL 讀取 .json 檔
 gdf = gpd.read_file(url)
 
 # (選用) 驗證是否成功
-st.dataframe(gdf.head())
+st.dataframe(gdf.())
 
 # --- 2. 建立地圖 ---
 m = leafmap.Map(center=[0, 0])
@@ -22,10 +23,8 @@ m = leafmap.Map(center=[0, 0])
 # 使用 add_gdf()方法
 m.add_gdf(
 gdf,
-layer_name="地圖國界(Vector)",
+layer_name="路外停車資訊",
 style={"fillOpacity": 0, "color": "black", "weight": 0.5}, # 設為透明，只留邊界}
-# highlight=False 會關閉滑鼠懸停時的反白效果，
-# 並「同時」阻止工具提示(Tooltip)的觸發
 highlight=False
 )
 # 加入圖層控制器(右上角)
